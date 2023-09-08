873f4f94daf88ab5e0f8e042459b3c8e51b575cc
Yerko <yerko@agendapro.com>
2023-09-08 00:21:31 -0300

    feat: wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react'
+import './App.css'
 
-import { skills } from "./data/EO4/landsknecht";
+import { skills } from './data/EO4/landsknecht'
 
 function App() {
-  return (
-    <>
-      <h1>Etrian Oddyssey Skills detail</h1>
-      {skills.map((skill: any) => (
-        <div className="card">{skill.name_en}</div>
-      ))}
-    </>
-  );
+   const [currentClass, setCurrentClass] = useState('')
+   const [openSection, setOpenSection] = useState(false)
+   const [openSkill, setOpenSkill] = useState('')
+   return (
+      <>
+         <h1>Etrian Oddyssey Skills detail</h1>
+         {currentClass === '' && (
+            <>
+               <div
+                  className="box"
+                  onClick={() => setCurrentClass('landsknecht')}
+               >
+                  <h2>Landsknecht</h2>
+               </div>
+            </>
+         )}
+         {currentClass === 'landsknecht' && (
+            <>
+               {skills.map((skillCat: any) => (
+                  <>
+                     <div
+                        class="box"
+                        onClick={() => setOpenSection(!openSection)} 
+                     >
+                        <p>{skillCat.name}</p>
+                     </div>
+                     {openSection && (
+                        <>
+                           {skillCat.skills.map((skill) => (
+                              <>
+                                <div class="box" onClick={()=> setOpenSkill(skill.name_en)}>
+                                  <p>{skill.name_en}</p>
+                                </div>
+                                {openSkill === skill.name_en && (
+                                  <div class="box">
+                                  <p>{skill.description}</p>
+                                </div>
+                                )}
+                              </>
+                            ))}
+                        </>
+                      )}
+                  </>
+                ))}
+            </>
+         )}
+      </>
+   )
 }
 
-export default App;
+export default App
